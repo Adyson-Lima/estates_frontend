014d50ecbce16a8e10201e031e9b12df688a6f0c
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-18 14:33:13 -0300

    24° commit - logica read implementada

--- src/pages/Estates/index.js
+++ src/pages/Estates/index.js
@@ -1,8 +1,17 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-// import api from '../../services/api';
+import api from '../../services/api';
 
 export default function Estates(){
+
+  const[my_estates, setEstates] = useState([]);
+  const navigate = useNavigate();
+
+  // read, busca todos os registros na api
+  useEffect(() => {
+    api.get('api/v1/estates',{})
+    .then(response => {setEstates(response.data)})
+  }, []);
 
   return(
 
@@ -18,27 +27,29 @@ export default function Estates(){
         <table data-testid="mytable" className="table table-hover">
           <thead>
             <tr>
-              <th scope="col">##</th>
-              <th scope="col">##</th>
-              <th scope="col">##</th>
-              <th scope="col">##</th>
+              <th scope="col">ID</th>
+              <th scope="col">Nome</th>
+              <th scope="col">Capital</th>
+              <th scope="col">Ações</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th scope="row"></th>
-                <td></td>
-                <td></td>
-                <td>
+            {my_estates.map(estate => (
+              <tr key={estate.id}>
+                <th scope="row">{estate.id}</th>
+                  <td>{estate.name}</td>
+                  <td>{estate.capital}</td>
+                  <td>
 
-                  <button data-testid="mybtn1" type="button"
-                  className="btn btn-outline-info">Editar</button>
+                    <button data-testid="mybtn1" type="button"
+                    className="btn btn-outline-info">Editar</button>
 
-                  <button data-testid="mybtn2" type="button"
-                  className="btn btn-outline-danger">Excluir</button>
+                    <button data-testid="mybtn2" type="button"
+                    className="btn btn-outline-danger">Excluir</button>
 
-                </td>
-            </tr>
+                  </td>
+              </tr>
+            ))}            
           </tbody>
         </table>
 
